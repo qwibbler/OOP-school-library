7b5ba98f57519cb8a66483fafe8a9a1433ccc5f9
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-02-28 14:03:42 -0600

    Add class Person

---
+++ project.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_reader :id, :name
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission || true
+  end
+
+  def can_use_services?
+    return false unless is_of_age? || @parent_permission
+
+    true
+  end
+
+  private
+
+  def is_of_age?
+    return true if @age >= 18
+
+    false
+  end
+end
+
+john = Person.new(18)
+p john.can_use_services?
