a891307cd07048a67aecbf22398a435620705446
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-03-03 19:04:00 -0600

    Take methods out of class

--- main.rb
+++ main.rb
@@ -12,22 +12,7 @@ class App
     @books = []
   end
 
-  def welcome
-    puts [
-      '',
-      'Please choose an option by entering a number',
-      '1 - List all books',
-      '2 - List all people',
-      '3 - Create a person',
-      '4 - Create a book',
-      '5 - Create a rental',
-      '6 - List all rentals for a given person id',
-      '7 - Exit'
-    ]
-    option_chosen(gets.chomp)
-  end
-
-  def option_chosen(option) # rubocop:disable Metrics/CyclomaticComplexity
+  def option_chosen(option)
     case option
     when '1'
       all_books
@@ -41,37 +26,33 @@ class App
       rent_book
     when '6'
       list_rents
-    when '7', 'q', 'quit', 'exit'
-      end_program
     else
-      returns wrong_option
+      wrong_option
     end
-  end
-
-  def all_books
-    puts 'Books List: '
-    @books.each_with_index { |book, index| puts print_book(book, index) }
-    puts 'No books' if @books.empty?
-    welcome
   end
 
   def print_book(book, index = nil)
     "#{index + 1}) #{book.title} by #{book.author}"
   end
 
-  def all_people
-    puts 'People List: '
-    @people.each_with_index { |person, index| puts print_person(person, index) }
-    puts @people.empty? ? 'No people' : ''
-    welcome
+  def all_books
+    puts 'Books List: '
+    @books.each_with_index { |book, index| puts print_book(book, index) }
+    return 'No books' if @books.empty?
   end
 
   def print_person(person, index = 1)
     "#{index + 1}) [#{person.class.name}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
   end
 
+  def all_people
+    puts 'People List: '
+    @people.each_with_index { |person, index| puts print_person(person, index) }
+    return 'No people' if @people.empty?
+  end
+
   def make_person
-    puts 'Do you want to create a student (1) or a teacher (2)?'
+    print 'Do you want to create a student (1) or a teacher (2)? '
     opt = gets.chomp
     return wrong_option unless %w[1 2].include? opt
 
@@ -86,19 +67,17 @@ class App
   def create_student(age, name)
     print 'Has Parent Permission? [Y/N]: '
     pp = gets.chomp
-    returns wrong_option unless %w[y n].include?(pp.downcase)
+    return wrong_option unless %w[y n].include? pp.downcase
 
     @people << Student.new(age, name, parent_permission: pp.downcase == 'y')
-    puts 'Student created successfully'
-    welcome
+    'Student created successfully'
   end
 
   def create_teacher(age, name)
     print 'Specialization: '
     spec = gets.chomp
     @people << Teacher.new(spec, age, name)
-    puts 'Teacher created successfully'
-    welcome
+    'Teacher created successfully'
   end
 
   def make_book
@@ -108,8 +87,29 @@ class App
     author = gets.chomp
     book = Book.new(title, author)
     @books << book
-    puts 'Book created successfully'
-    welcome
+    'Book created successfully'
+  end
+
+  def select_book
+    puts 'Select a book from the following selection by number: '
+    @books.each_with_index { |book, index| puts print_book(book, index) }
+    book = gets.chomp
+    book = book.to_i
+    return 'wrong_option' unless book.positive? && book <= @books.length
+
+    puts "You have chosen (#{print_book(@books[book - 1], book - 1)}."
+    @books[book - 1]
+  end
+
+  def select_person
+    puts 'Select a person from the following selection by number (not id): '
+    @people.each_with_index { |person, index| puts print_person(person, index) }
+    person = gets.chomp
+    person = person.to_i
+    return 'wrong_option' unless person.positive? && person <= @people.length
+
+    puts "You have chosen (#{print_person(@people[person - 1], person - 1)}."
+    @people[person - 1]
   end
 
   def rent_book
@@ -122,46 +122,27 @@ class App
       return make_person
     end
     book = select_book
+    return wrong_option if book == 'wrong_option'
+
     person = select_person
+    return wrong_option if person == 'wrong_option'
+
     print 'Date: '
     date = gets.chomp
     Rental.new(book, person, date)
-    puts 'Book successfully rented'
-    welcome
-  end
-
-  def select_book
-    puts 'Select a book from the following selection by number: '
-    @books.each_with_index { |book, index| puts print_book(book, index) }
-    book = gets.chomp
-    book = book.to_i
-    returns wrong_option unless book.positive? && book <= @books.size
-
-    puts "You have chosen (#{print_book(@books[book - 1], book - 1)}."
-    @books[book - 1]
-  end
-
-  def select_person
-    puts 'Select a person from the following selection by number (not id): '
-    @people.each_with_index { |person, index| puts print_person(person, index) }
-    person = gets.chomp
-    person = person.to_i
-    returns wrong_option unless person.positive? && person <= @people.size
-
-    puts "You have chosen (#{print_person(@people[person - 1], person - 1)}."
-    @people[person - 1]
+    'Book successfully rented'
   end
 
   def list_rents
     print 'Enter person ID: '
     id = gets.chomp
     renting_person = @people.find { |person| person.id == id.to_i }
-    wrong_option unless renting_person
-    renting_person.rents.each do |rent|
-      puts "Date: #{rent.date}, Book: #{rent.book.title} by #{rent.book.author}"
+    return wrong_option unless renting_person
+
+    renting_person.rents.each_with_index do |rent, index|
+      puts "#{index + 1}) Date: #{rent.date}, Book: #{rent.book.title} by #{rent.book.author}"
     end
-    puts 'No Rental Information' if renting_person.rents.empty?
-    welcome
+    return 'No Rental Information' if renting_person.rents.empty?
   end
 
   def end_program
@@ -170,9 +151,28 @@ class App
   end
 
   def wrong_option
-    puts 'That was not a valid option'
-    welcome
+    'That was not a valid option'
   end
+end
+
+def options(app)
+  puts [
+    '',
+    'Please choose an option by entering a number',
+    '1 - List all books',
+    '2 - List all people',
+    '3 - Create a person',
+    '4 - Create a book',
+    '5 - Create a rental',
+    '6 - List all rentals for a given person id',
+    '7 - Exit'
+  ]
+  opt = gets.chomp
+  puts
+  return app.end_program if %w[7 q quit exit].include? opt
+
+  puts app.option_chosen(opt)
+  options(app)
 end
 
 def main
@@ -182,7 +182,7 @@ def main
     ''
   ]
   app = App.new
-  app.welcome
+  options(app)
 end
 
 main
